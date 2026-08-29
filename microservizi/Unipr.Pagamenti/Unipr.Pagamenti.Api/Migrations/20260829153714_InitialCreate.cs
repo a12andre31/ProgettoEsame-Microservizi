8b88e90142520bd3819c6cff6e32e939a7db83f2
a12andre31 <andreacrisostomo25@gmail.com>
2026-08-29 17:45:51 +0200

    update ports

---
+++ microservizi/Unipr.Pagamenti/Unipr.Pagamenti.Api/Migrations/20260829153714_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unipr.Pagamenti.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContiClienti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    Saldo = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContiClienti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionalOutboxList",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tabella = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Messaggio = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionalOutboxList", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContiClienti");
+
+            migrationBuilder.DropTable(
+                name: "TransactionalOutboxList");
+        }
+    }
+}
